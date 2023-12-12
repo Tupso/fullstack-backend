c01d4ac08fe474fec5cefe279cf89c6fdd304525
Tupso <74798365+Tupso@users.noreply.github.com>
2023-12-12 09:56:35 +0100

    guion antes de nombre añadido

--- persistencia.py
+++ persistencia.py
@@ -3,5 +3,5 @@
 def guardar_pedido(nombre, apellidos):
     """" Escribir el .txt """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-" + nombre + " " + apellidos + "\n")
         file.close()
